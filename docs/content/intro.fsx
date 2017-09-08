2905feab549bf3b26470ba6b04f135fa2ab9c733
ReedCopsey <reed@reedcopsey.com>
2017-09-08 10:14:29 -0700

    Updated docs

--- docs/content/intro.fsx
+++ docs/content/intro.fsx
@@ -5,55 +5,16 @@
 #r "C:/Program Files (x86)/Reference Assemblies/Microsoft/Framework/.NETFramework/v4.6.1/Facades/System.Runtime.dll"
 
 (**
-A Brief Introduction to Gjallarhorn
+A Brief Introduction to Gjallarhorn.Bindable
 ========================
 
-Gjallarhorn is centered around two core types - Signals and Mutables.
+Gjallarhorn.Bindable provides a new UI architecture for XAML platforms
 
-A signal represents a value that changes over time, and has the following properties:
-- A current value, which can always be fetched
-- A mechanism to signal changes to subscribers that the value has been updated
+The goal is to keep the best of XAML (decoupled view, flexible UI, high performance rendering, etc), 
+while allowing a fully immutable data model, pure functions for updates on the model, and a clean 
+architecture to be used for representing the application.
 
-A Mutable is a holder for mutable variables, similar to a reference cell.  They are typically created via Mutable.create:
-
-*)
-#r "Gjallarhorn.dll"
-open Gjallarhorn
-
-// Create a mutable variable
-let variable = Mutable.create "Foo"
-printfn "hello = %s" variable.Value
-variable.Value <- "Bar"
-printfn "hello = %s" variable.Value
-
-(**
-
-In this simple example, we show creating, reading from, and updating a Mutable.
-
-Mutable variables in Gjallarhorn have some distict features:
-
-- Very low memory overhead: When created, a mutable like the one above has no memory overhead above that of a standard reference cell. It is effectively nothing but a thin wrapper around the current value, without any extra fields.
-- They expose themselves as a signal - there is always a current value and they can notify subscribers of changes
-
-Once your core data is held within a Mutable or pulled in via something that provides a signal, you can use the Signal module to transform the data
-
-*)
-
-// Create mutable variables
-let first = Mutable.create ""
-let last = Mutable.create ""
-// Map these two variables from a first and last name to a full name
-let full = Signal.map2 (fun f l -> f + " " + l) first last
-
-printfn "initial = %s" full.Value
-first.Value <- "Reed"
-last.Value <- "Copsey"
-// Prints: "Hello Reed Copsey!"
-printfn "Hello %s!" full.Value
-
-
-(** 
-
-Next, we'll go into [more details about signals](signals.html).
+This is done by building up on the Gjallarhorn project to create a reactive, data flow oriented 
+framework for XAML Binding.
 
 *)
