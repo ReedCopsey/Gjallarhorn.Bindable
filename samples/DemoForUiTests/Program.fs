9ce487a57fbbcf977f8aee8f2d10744d8616ed0f
FoggyFinder <FoggyFinder@yandex.ua>
2019-03-18 19:51:20 +0200

    Solve #21 (#22)
    * attempt to solve #21
    
    * UI test simplification, clean up
    
    * clean up configurations in sln files
    
    * add demo ui test project back to main sln, fix build
    
    * remove white-spaces
    
    * revert "clean up configurations in sln files"
    
    * remove separate sln file for tests
    
    * using EmbeddedResource instead of Resource for file visibility

---
+++ samples/DemoForUiTests/Program.fs
@@ -0,0 +1,55 @@
+﻿open System
+
+[<RequireQualifiedAccess>]
+module Views = 
+    open FsXaml
+
+    type MainWin= XAML<"MainWindow.xaml">
+
+open Gjallarhorn.Bindable
+open Gjallarhorn.Bindable.Framework
+
+module Program = 
+    type Cell = { Value : int }
+    type Model = { Items : Cell list; IsOrdered : bool }
+
+    type Msg = | Move of Cell
+    type CellMsg = | Click
+    let update msg model =
+        match msg with
+        | Move value ->
+            let nitems = value :: List.except [ value ] model.Items
+            let sorted = nitems |> List.sort
+            if nitems = sorted then { Items = nitems ; IsOrdered = true }
+            else { model with Items = nitems }
+    
+    type ViewModel = 
+        {
+            Model : Cell
+            Click : VmCmd<CellMsg>
+        }
+
+    let init = { Items = [ 3; 1; 2 ] |> List.map (fun i -> { Value = i }); IsOrdered = false }
+    let cell = { Value = 1 }
+    let d = { Model = cell ; Click = Vm.cmd CellMsg.Click }
+    
+    let cellComponent =
+        Component.create [
+            <@ d.Model @> |> Bind.oneWay id
+            <@ d.Click @> |> Bind.cmd
+        ]
+    let bindToSource =
+        Component.create [
+            <@ init.Items @> |> Bind.collection (fun m -> m.Items) cellComponent (snd >> Msg.Move)
+            <@ init.IsOrdered @> |> Bind.oneWay (fun m -> m.IsOrdered)
+        ]         
+
+    let applicationCore = Framework.application init update bindToSource Nav.empty
+
+open Gjallarhorn.Wpf
+
+[<STAThread>]
+[<EntryPoint>]
+let main _ =          
+    Framework.RunApplication (Navigation.singleViewFromWindow Views.MainWin, Program.applicationCore)
+    1
---
+++ tests/Gjallarhorn.Bindable.Tests.Ui/Library1.fs
@@ -0,0 +1,31 @@
+﻿namespace Gjallarhorn.Bindable.Tests.Ui
+
+module Collection = 
+    open NUnit.Framework
+    open Gu.Wpf.UiAutomation
+    open System.Windows
+    open System.IO
+    open System
+
+    let appPath = @"samples\DemoForUiTests\bin\Debug\net471\DemoForUiTests.exe"
+
+    [<Test>]
+    let ``issue: #21 - InvalidOperationException: Added item does not appear at given index '1'``() = 
+        let current = TestContext.CurrentContext.WorkDirectory
+        let path = Path.GetFullPath(Path.Combine(current, "..", "..", "..", "..", "..", appPath))
+        Assert.True(File.Exists(path))
+
+        use application = Application.AttachOrLaunch(path)
+        let window = application.GetMainWindow(new Nullable<TimeSpan>(TimeSpan.FromSeconds(15.0)))
+
+        Assert.AreEqual("Ui tests", window.Title)
+        
+        let btn = window.FindButton("1")
+        btn.Click()
+
+        // to make sure that there is no any exception that kills app
+        Wait.For(TimeSpan.FromSeconds(5.0))
+        Assert.IsFalse(application.HasExited)
+
+        application.Close() |> ignore
+        Assert.IsTrue(application.HasExited)
